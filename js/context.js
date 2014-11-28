fb92f1b1dc6ac0937b1b042fa602a002b58b1046
atvinovskajanina <atvinovskayan@gmail.com>
2014-11-28 12:49:52 +0300

    edit posts,add archive

--- js/context.js
+++ js/context.js
@@ -1,10 +1,39 @@
-function print_todays_date() {
-var d = new Date(); // Получаем текущую дату и время
-document.write(d.toLocaleString()); // Вставляем это в документ
-}
+var posts =[
+		{
+			"title": "Как почистить матрицу фотоаппарата",
+			"date":"2014-11-27",
+			"content": "Здесь контекст статьи 1"
+		},
+		{
+			"title": "3 объектива Canon для съемки в путешествии",
+			"date":"2014-11-10",
+			"content": "Здесь контекст статьи 2"
+		},
+		{
+			"title": "Как сделать удачное селфи",
+			"date":"2014-10-15",
+			"content": "Здесь контекст статьи 3"
+		}
+	]
+var $ = document.querySelector.bind(document)
 
-function print_array() {
-var fruits = ["манго", "банан", "вишня", "персик"];
-for(var i = 0; i < fruits.length; i++)
-document.write(fruits[i].toLocaleString()); 
+var containerEl = $('.posts-container')
+
+// Render Fn
+var render = function(arr) {
+  return arr.reduce(function(previousValue, currentValue, index, array) { return previousValue +
+																								
+																								'<ul>'+
+																									//insertEl('.blog-post-title',currentValue.title)+
+																									'<li>' + currentValue.title + '</li>'+
+																									'<li>' + currentValue.date + '</li>'+
+																									'<li>' + currentValue.content + '</li>'+
+																								'</ul>'
+																		  },'');
 }
+//var insertEl = function(el,value){
+	//return $(el).innerHTML = value;
+//}
+
+// Init
+//containerEl.innerHTML = render(posts)
