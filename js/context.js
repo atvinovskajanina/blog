307f62162483008f8dda1e7dc2529f79f5bf8dc2
atvinovskajanina <atvinovskayan@gmail.com>
2014-12-01 23:36:52 +0300

    Add filter,content of table

--- js/context.js
+++ js/context.js
@@ -1,35 +1,42 @@
 var posts =[
 		{
 			"title": "Как почистить матрицу фотоаппарата",
-			"date":"27-11-2014",
-			"content": "Здесь контекст статьи 1"
+			"date":"01-12-2014",
+			"content": "Чистка матрицы фотоаппарата представляет собой довольно тонкое и деликатное занятие, которое требует терпения и понимания уязвимой структуры самого механизма. Некоторые модели камер, к примеру, устройства Canon, могут самостоятельно провести удаление пыли благодаря встроенной функции очистки матрицы, или, по крайней мере, не позволят ей испортить ваши снимки."
 		},
 		{
 			"title": "3 объектива Canon для съемки в путешествии",
-			"date":"10-11-2014",
-			"content": "Здесь контекст статьи 2"
+			"date":"23-11-2014",
+			"content": "Единственные три объектива Canon, необходимые для съемки во время путешествий в любую точку мира, – это сверхширокоугольный объектив (“рыбий глаз”/фишай), 50mm и 135mm (или аналогичный телефото)."
 		},
 		{
 			"title": "Как сделать удачное селфи",
-			"date":"15-10-2014",
-			"content": "Здесь контекст статьи 3"
+			"date":"11-11-2014",
+			"content": "Вошедшее в моду селфи является отличным и веселым способом показать всему миру свою самоуверенность, индивидуальность и чувство стиля. "
 		},
 		{
 			"title": "Полезные советы",
+			"date":"23-10-2014",
+			"content": "Обычно в камере предусмотрено два или три режима фокусировки. В каждом конкретном случае они дают аппарату инструкцию, как нужно фокусироваться, а зависеть это будет от того, что вы снимаете"
+		},
+		{
+			"title": "Лучшие фото-приложения для iPhone, создающие эффект ретро",
 			"date":"03-10-2014",
-			"content": "Здесь контекст статьи 4"
+			"content": "Сегодня, когда рынок фототехники переполнен мегапиксельными фотокамерами высокого разрешения, человеку не хватает излучающих тепло, зернистых и далеких от идеала снимков эпохи аналоговых фотокамер и фотопленок. Ностальгия – часть человеческой натуры"
 		}
 	]
 var $ = document.querySelector.bind(document)
 var containerEl = $('.posts-container')
+var sidebarEl = $('.sidebar-module ol')
 
 var render = function(arr) {
   return arr.reduce(function(previousValue, currentValue, index, array) { return previousValue +
-																							//	'<li>'+
-																									'<h3>'+currentValue.title+'</h3>'+
+																								'<li id ='+index+'>'+
+																									
+																									'<h4>'+currentValue.title+'</h4>'+
 																									'<p>' + currentValue.date + '</p>'+
 																									'<p>' + currentValue.content + '</p>'//+
-																								//'</li>'
+																								'</li>'
 																							
 																		  },'');
 }
@@ -37,15 +44,39 @@ var render = function(arr) {
 // Init
 containerEl.innerHTML = render(posts);
 
+function parseDate(dateString){
+
+return  new Date (dateString.replace(/(\d+)-(\d+)-(\d+)/, '$2/$1/$3')).getTime();
+}
+
 function loadbtn(event){
-			var filterBegin = new Date(($('#filterBegin').value).replace(/(\d+)-(\d+)-(\d+)/, '$2/$1/$3')).getTime();
-			var filterEnd =new Date(($('#filterEnd').value).replace(/(\d+)-(\d+)-(\d+)/, '$2/$1/$3')).getTime();
+			var filterBegin = parseDate($('#filterBegin').value);
+			var filterEnd =parseDate($('#filterEnd').value);
 			var postslist = posts.slice()
 			function isEnough(element) {
-				return (new Date((element.date).replace(/(\d+)-(\d+)-(\d+)/, '$2/$1/$3')).getTime() >= filterBegin)&&(new Date((element.date).replace(/(\d+)-(\d+)-(\d+)/, '$2/$1/$3')).getTime() <= filterEnd)
+				return (parseDate(element.date) >= filterBegin)&&(parseDate(element.date) <= filterEnd)
 			}
 			var filterlist=postslist.filter(isEnough)
-			containerEl.innerHTML = render(filterlist);
+		
+			if (filterlist.value == null){
+				containerEl.innerHTML="Ничего не найдено. Повторите!"
+			}else{
+				containerEl.innerHTML = render(filterlist);
+			}
 	}
 
- 
+function loadpost(index){
+	containerEl.innerHTML = '<li id ='+index+'>'+
+																									
+																									'<h4>'+posts[index].title+'</h4>'+
+																									'<p>' + posts[index].date + '</p>'+
+																									'<p>' + posts[index].content + '</p>'//+
+																								'</li>'
+}
+var render2 = function(arr2) {
+  return arr2.reduce(function(previousValue, currentValue, index, array) { return previousValue +
+																								'<li><a onclick="loadpost('+index+')">'+currentValue.title+'</a></li>'
+																								
+																		  },'');
+}
+ sidebarEl.innerHTML = render2(posts);
