235bcba9d212f16a17471ef94f1131ae4517f0ec
unknown <Nina_Atvinouskaya@EPBYGOMW1004.gomel.epam.com>
2015-01-05 17:45:34 +0300

    Edited pagination

--- js/pagination.js
+++ js/pagination.js
@@ -1,9 +1,9 @@
 $(function(){
-		$('.liPaginate').liPaginate({
-			easing:'easeOutQuart', //http://gsgd.co.uk/sandbox/jquery/easing/
-			duration: 1000, // A string or number determining how long the animation will run
-			effect:'simple', //simple, fade, hSlide, vSlide
-			pagePos:'after', //after, before, double
-			pageHeight:'auto'//[px] - height of paginate item, ['auto'] - detect window height
-		});
+	$('.liPaginate').liPaginate({
+		easing:'easeOutQuart', //http://gsgd.co.uk/sandbox/jquery/easing/
+		duration: 1000, // A string or number determining how long the animation will run
+		effect:'simple', //simple, fade, hSlide, vSlide
+		pagePos:'after', //after, before, double
+		pageHeight:'300'//[px] - height of paginate item, ['auto'] - detect window height
+	});
 });
